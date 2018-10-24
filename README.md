9b9fd6ae23b88c3dfd1b41d32f59b813e15d0328
Jussi Kinnula <jussi.kinnula@wunderdog.fi>
2018-10-24 09:08:06 +0300

    Bump version + minor fixes

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # remote-log-websocket
 
+## Usage
 
+### Installation
+
+```bash
+$ npm install --save remote-log-websocket
+```
+
+### Client
+
+On your application, just use the client:
+
+```js
+require('remote-log-websocket')('ws://localhost:12345')
+```
+
+### Server
+
+From command line:
+
+```bash
+REMOTE_LOG_HOST=0.0.0.0 REMOTE_LOG_PORT=12345 remote-log
+```
+
+...or from your own NodeJS wrapper:
+
+```
+require('remote-log-websocket/server')()
+```
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,28 @@
 import { server as WebSocketServer } from 'websocket'
 import * as express from 'express'
 
-const PORT = process.env.REMOTE_LOG_PORT ? parseInt(process.env.REMOTE_LOG_PORT, 10) : 12345
-const HOST = process.env.REMOTE_LOG_HOST || 'localhost'
-const app = express()
-const server = app.listen(PORT, HOST, () => {
-  console.log(`Remote log server listening in http://${HOST}:${PORT}`)
-})
-
-// create the WebSocketServer
-const websocketServer = new WebSocketServer({ httpServer: server })
-
-// WebSocket server
-websocketServer.on('request', (request) => {
-  const connection = request.accept(null, request.origin)
-  console.log('Client connected')
-
-  connection.on('message', (message) => {
-    if (message.type === 'utf8') {
-      console.log(message.utf8Data)
-    }
+export default (host: string, port: number) => {
+  const app = express()
+  const server = app.listen(port, host, () => {
+    console.log(`Remote log server listening in http://${host}:${port}`)
   })
 
-  connection.on('close', (connection) => {
-    console.log('Client disconnected')
+  // create the WebSocketServer
+  const websocketServer = new WebSocketServer({ httpServer: server })
+
+  // WebSocket server
+  websocketServer.on('request', (request) => {
+    const connection = request.accept(null, request.origin)
+    console.log('Client connected')
+
+    connection.on('message', (message) => {
+      if (message.type === 'utf8') {
+        console.log(message.utf8Data)
+      }
+    })
+
+    connection.on('close', (connection) => {
+      console.log('Client disconnected')
+    })
   })
-})
+}
